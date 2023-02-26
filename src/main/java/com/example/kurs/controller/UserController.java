3a54107a2303c1a121d8c20f17aec3e5added8a1
Anton-Shabalov <anton.shabalov@icloud.com>
2023-02-26 21:27:40 +0300

    .

--- src/main/java/com/example/kurs/controller/UserController.java
+++ src/main/java/com/example/kurs/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/user")
 public class UserController {
     @Autowired
@@ -22,19 +23,18 @@ public class UserController {
 
     @PostMapping("/add-recipe")
     public ResponseEntity<String> createUser(@RequestBody RecipeDto requestRecipe, @RequestHeader HttpHeaders header) {
-        String jwt=header.getFirst("Authorization");
+        String jwt = header.getFirst("Authorization");
         String token = jwt.substring(7);
-        Long userId= jwtTokenProvider.getId(token);
-        if(userService.existsById(userId)){
-            Boolean pushed = recipeService.pushNewResive(requestRecipe,userId);
-            if (!pushed){
-                    return ResponseEntity.status(423).body("Failed to create recipe");
+        Long userId = jwtTokenProvider.getId(token);
+        if (userService.existsById(userId)) {
+            Boolean pushed = recipeService.pushNewResive(requestRecipe, userId);
+            if (!pushed) {
+                return ResponseEntity.status(423).body("Failed to create recipe");
             }
         } else {
             return ResponseEntity.status(422).body("User doesn't exist");
         }
 
-
         return ResponseEntity.status(200).body("Success");
-}
+    }
 }

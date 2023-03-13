857e167d78238bdd0397a9c066c4042e1edb2ccb
Anton-Shabalov <anton.shabalov@icloud.com>
2023-03-13 19:59:56 +0300

    Рабочая версия, осталось поднять на вайлдфлай

--- src/main/java/com/example/kurs/BLSSApplication.java
+++ src/main/java/com/example/kurs/BLSSApplication.java
@@ -2,8 +2,10 @@ package com.example.kurs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class BLSSApplication {
 
     public static void main(String[] args) {

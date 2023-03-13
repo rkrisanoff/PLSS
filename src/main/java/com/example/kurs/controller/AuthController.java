8861f01a5efef2692f13e7588bd014dc35b1abc2
Крисанов Роман Валерьевич <284712@niuitmo.ru>
2023-03-13 20:09:03 +0300

    basic-auth

--- src/main/java/com/example/kurs/controller/AuthController.java
+++ src/main/java/com/example/kurs/controller/AuthController.java
@@ -22,7 +22,7 @@ public class AuthController {
     private UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@Valid @RequestBody SignupDto signupDto) throws UserAlreadyExistsException, EmailAlreadyExistsException, SystemException {
+    public ResponseEntity<String> signup(@Valid @RequestBody SignupDto signupDto) throws UserAlreadyExistsException, EmailAlreadyExistsException {
         userService.register(signupDto);
         return ResponseEntity.ok("");
     }
--- src/main/java/com/example/kurs/security/UserDetailServiceImpl.java
+++ src/main/java/com/example/kurs/security/UserDetailServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +21,7 @@ public class UserDetailServiceImpl implements UserDetailsService {
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
+        System.out.println(user.getRole().getName());
         return User.withUsername(user.getUsername())
                 .password(user.getPassword())
                 .roles(user.getRole().getName())

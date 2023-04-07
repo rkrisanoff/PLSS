9e3040ada2c0c9a2a222e96855372142418142b2
Anton-Shabalov <anton.shabalov@icloud.com>
2023-04-07 18:24:58 +0300

    Готово к критике Ромы))

--- src/main/java/com/example/kurs/RabbitEmailAlert/config/RabbitConfiguration.java
+++ src/main/java/com/example/kurs/RabbitEmailAlert/config/RabbitConfiguration.java
@@ -6,21 +6,26 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableRabbit
 @Configuration
 public class RabbitConfiguration {
-
-    public static final String QUEUE_FOR_EMAIL = "queueForEmail";
-    public static final String EXCHANGE_NAME = "exchangeOne";
-    public static final String ROUTING_KEY = "recipeStatusUpdate";
+    @Value("${rabbitmq.QUEUE_FOR_EMAIL}")
+    private static String QUEUE_FOR_EMAIL;
+    @Value("${rabbitmq.EXCHANGE_NAME}")
+    private static String EXCHANGE_NAME;
+    @Value("${rabbitmq.ROUTING_KEY}")
+    private static String ROUTING_KEY;
+    @Value("${rabbitmq.HOST}")
+    private static String HOST;
 
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory =
-                new CachingConnectionFactory("localhost");
+                new CachingConnectionFactory(HOST);
         return connectionFactory;
     }
 
--- src/main/java/com/example/kurs/RabbitEmailAlert/service/RabbitService.java
+++ src/main/java/com/example/kurs/RabbitEmailAlert/service/RabbitService.java
@@ -4,6 +4,7 @@ import com.example.kurs.RabbitEmailAlert.DTO.Message;
 import com.example.kurs.RabbitEmailAlert.config.RabbitConfiguration;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +12,11 @@ public class RabbitService {
     @Autowired
     RabbitTemplate template;
 
+    @Value("${rabbitmq.EXCHANGE_NAME}")
+    private static String EXCHANGE_NAME;
+
     public void sendMailStatusChangeRecipe(String message) {
-        template.setExchange(RabbitConfiguration.EXCHANGE_NAME);
+        template.setExchange(EXCHANGE_NAME);
         template.convertAndSend(message);
     }
 
